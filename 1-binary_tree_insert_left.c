fae4dadf9635560c36dc7998a5cff1372d695a82
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-11-29 04:16:45 +0100

    Fix tasks 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,22 +12,20 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
-	if (!parent || !node)
+	if (!parent)
 		return (NULL);
 
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
+	node = binary_tree_node(parent, value);
+	if (!node)
+		return (NULL);
 
 	if (parent->left)
 	{
 		node->left = parent->left;
 		node->left->parent = node;
 	}
-
 	parent->left = node;
 
 	return (node);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,15 +12,14 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node;
 
-	if (!parent || !node)
+	if (!parent)
 		return (NULL);
 
-	node->n = value;
-	node->parent = parent;
-	node->left = NULL;
-	node->right = NULL;
+	node = binary_tree_node(parent, value);
+	if (!node)
+		return (NULL);
 
 	if (parent->right)
 	{
