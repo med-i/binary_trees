cebaa2aa9d7fe0b016ba2e2193ab611a37330398
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-11-29 10:03:41 +0100

    Add task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,4 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	left_height = 1 + binary_tree_height(tree->left);
+	right_height = 1 + binary_tree_height(tree->right);
+
+	return (left_height > right_height ? left_height : right_height);
 }
