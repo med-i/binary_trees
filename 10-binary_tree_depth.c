6b618ff42575be0bd6dadf79f28ad5b34b32a8ff
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-11-29 18:51:31 +0100

    Add task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,4 +8,12 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth;
+
+	if (!tree || !tree->parent)
+		return (0);
+
+	depth = 1 + binary_tree_depth(tree->parent);
+
+	return (depth);
 }
