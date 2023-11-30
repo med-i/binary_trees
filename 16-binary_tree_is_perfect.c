140dbe276b1b35bc055d416bf22777c71064b111
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-11-30 20:33:34 +0100

    Add task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,4 +8,15 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (!tree->left || !tree->right)
+		return (0);
+
+	return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
 }
