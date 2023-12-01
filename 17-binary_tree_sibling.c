63d5e1304e0fe7fee68d238fa3a7cf6c92029cc7
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-12-01 01:31:59 +0100

    Add task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,4 +9,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (!node || !node->parent)
+		return (NULL);
+
+	return (node->parent->left == node ? node->parent->right
+									   : node->parent->left);
+
+	return (NULL);
 }
